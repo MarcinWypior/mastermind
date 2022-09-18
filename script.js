35f56533b62c6794ff192a321d37503d41199f00
@Marcin1136 <marcinwypior1990@gmail.com>
2022-09-18 14:45:51 +0200

    przed przenoszeniem zmiany koloru do funcji

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
 let colors=6;
 let attempts=8;
 let slots=4;
-
+let currentRow=1;
+    
 console.log(colors+" kolorów do zgadywania \n");
 console.log(attempts+" prób \n");
 console.log(slots+" miesjsca na kulki \n");
@@ -12,7 +13,6 @@ var randomColors=[];
     {
         switch(Math.floor(Math.random() * colors)+1)
         {
-            
             case 1: 
             randomColors[i]="red";
             break;
@@ -35,13 +35,95 @@ var randomColors=[];
             randomColors[i]="cyan";
             break;
         }
-    }
+    } //Koniec losowania kolorów
                                  
     for(let i=0;i<randomColors.length;i++)
         console.log(randomColors[i]);
     
     
     
+
+    createNewP();
+    insertBallsToLastP();
+    createNewP();
+    insertBallsToLastP();
+    
+//    let lastP=document.getElementsByClassName("row").length
+//    console.log(document.getElementsByClassName("row")[lastP-1].children);
+//    document.getElementsByClassName("row")[lastP-1].children.forEach.removeEventListener("click",function);
+    
 });
+
+function createNewP(){
+    var newRow=document.createElement("p");
+    newRow.className="row";
+    newRow.setAttribute("data-rowNumber", 1);
+    document.body.appendChild(newRow);
+}
+
+function insertBallsToLastP(){
+    let lastRow=document.getElementsByClassName("row").length;
+    
+    for(let i=1;i<5;i++)
+    document.getElementsByClassName("row")[lastRow-1].appendChild(createBall(i));
+}
+
+function createBall(i) {
+    
+    
+    var newBall=document.createElement("span");
+                    newBall.className="ball";
+                    newBall.setAttribute("data-color", "gray");
+                    newBall.setAttribute("data-number", i);
+
+                    newBall.addEventListener('click', function() {
+            //console.log(i);
+                        
+
+              if(this.dataset.color=="gray")
+              {
+                this.style.backgroundColor="red";//1
+                this.dataset.color="red";
+              }
+
+              else if(this.dataset.color=="red")
+              {
+                this.style.backgroundColor="yellow";//2
+                this.dataset.color="yellow";
+              }
+
+              else if(this.dataset.color=="yellow")
+              {
+                this.style.backgroundColor="green";//3
+                  this.dataset.color="green";
+              }
+
+              else if(this.dataset.color=="green")
+              {
+                      this.style.backgroundColor="blue";//4
+                      this.dataset.color="blue";
+              }
+              else if(this.dataset.color=="blue")
+              {          
+                        this.style.backgroundColor="orange";//5
+                        this.dataset.color="orange";      
+              }
+              else if(this.dataset.color=="orange")
+              {
+                        this.style.backgroundColor="purple";//6
+                        this.dataset.color="purple";
+            
+              }
+              else if(this.dataset.color=="purple")
+              {
+                this.style.backgroundColor="red";//1
+                this.dataset.color="red";
+              }
+                    });
+  return newBall;
+};
+
+
+
 
 
